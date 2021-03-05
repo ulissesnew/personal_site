6c981e82eaf74b9149b2d1cb85d056dfac2d3237
ulissesnew <ulisses1024073@gmail.com>
2021-03-05 09:59:00 -0300

    update style in Skills page

--- src/pages/Skills/BackgroundSkills/BackgroundSkills.js
+++ src/pages/Skills/BackgroundSkills/BackgroundSkills.js
@@ -12,7 +12,7 @@ const Background = ({children}) => {
             <div className="d-silver-circle-right"></div>
             <div className="banana-skills"></div>
             <div className="silver-circle-right"></div>
-            <div className="m-blue-circle-skills"></div>
+            <div className="d-orange-circle-skills"></div>
             <div className="orange-pizza-skills2"></div>
             <div className="yellow-banana-skills"></div>
          
